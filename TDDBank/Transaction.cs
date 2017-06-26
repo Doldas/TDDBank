c0bf9ebd072b27a542d46a21ed6c3d9c2651790e
Gommash <a.lekedal@gmail.com>
2017-06-26 16:38:17 +0200

    bank

---
+++ TDDBank/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBank
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public double Money { get; set; }
+        public Category Category { get; set; }
+        public int Account_ID { get; set; }
+        public Account Account { get; set; }
+        public Account Reciever { get; set; }
+    }
+    public enum Category
+    {
+        Added,
+        Payment
+    }
+}
---
+++ TDDBank/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBank
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FullName {get; set;}
+        public string Contact { get; set; }
+        public int CountAccounts { get { return Accounts.Count(); } }
+        public List<Account> Accounts { get; set; }
+    }
+}
